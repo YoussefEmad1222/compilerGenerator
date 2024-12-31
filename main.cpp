36abe7185fe801c34a19fe734670c27dc34756ce
Hesham Youssef <hesham_ahmed2002002@yahoo.com>
2024-12-31 08:21:47 +0200

    added parser

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "phase 1/regexFileReader.cpp"
 #include "phase 1/NFAcreation.cpp"
 #include "phase 2/firstFollowCalculator.cpp"
-#include "phase 2/LeftRecursionElimination.cpp"
+#include "phase 2/leftRecursionElimination.cpp"
 #include "phase 2/leftFactoring.cpp"
 #include "phase 2/grammarFileParser.cpp"
 #include "phase 2/PredictiveParsingTable.cpp"
+#include "phase 2/parser.hpp"
 
 using namespace std;
 namespace fs = filesystem;
+
+void printTokens(const vector<pair<string, string>> &tokens) {
+    cout << "Tokens:" << endl;
+    for (const auto &token : tokens) {
+        cout << "Lexeme: " << token.first << ", Type: " << token.second << endl;
+    }
+}
 
 int main() {
     //  processLexicalAnalysis("../input/regex_rules.txt", "../input/input.txt", "../output/tokens.txt");
@@ -32,6 +40,18 @@ int main() {
     PredictiveParsingTable ppt = PredictiveParsingTable(lf.gfp->grammar, lf.gfp->nonTerminals, first, follow);
     ppt.constructTable();
     ppt.printTable();
+
+    // Read tokens from the file
+    string tokenFile = "output/tokens.txt";
+    vector<pair<string, string>> tokens = readTokens(tokenFile);
+
+    // Add an end-of-input token
+    tokens.emplace_back("$", "$");
+    printTokens(tokens);
+
+    // Parser
+    Parser parser = Parser(ppt, lf.gfp->nonTerminals);
+    parser.parse(tokens);
 
     return 0;
 }
